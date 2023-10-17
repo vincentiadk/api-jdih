ea7428e7cc4efffbe87e188b7aff880753eedec1
vincentiadk <50853015+vincentiadk@users.noreply.github.com>
2023-10-17 17:53:26 +0700

    Update PeraturanController.php

--- app/Http/Controllers/PeraturanController.php
+++ app/Http/Controllers/PeraturanController.php
@@ -35,7 +35,7 @@ class PeraturanController extends Controller
         $req_all = $request->all();
         foreach($req_all as $key=>$val){
             if($key != 'limit' && $key != 'page') {
-                $q->where($key, '%' . $val . '%');
+                $q->where($key, 'LIKE', '%' . $val . '%');
             }
         }
         $return["total"] = $q->count();
