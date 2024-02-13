5213b63926d0e8b67e86d7de609af3d5b2149036
vincentiadk <50853015+vincentiadk@users.noreply.github.com>
2024-02-13 09:20:57 +0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ $router->get('/token', 'TokenController@getToken');
 $router->get('/peraturan/file/{id_peraturan}', 'PeraturanController@getFile');
 $router->get('/galeri/file/{id}', 'GaleriController@getFile');
 $router->get('/berita/file/{id}', 'BeritaController@getFile');
+$router->get('/rancangan/file/{id}', 'RancanganPeraturanController@getFile');
 $router->group(['middleware'=> 'auth'], function($router) {
     $router->get('/list-peraturan', 'PeraturanController@getListPeraturan');
     $router->get('/peraturan/{id_peraturan}/detail', 'PeraturanController@getDetailPeraturan');
