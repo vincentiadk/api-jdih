ce49b9cb17ef9e67010590df56a5ca6c75c00471
vincentiadk <50853015+vincentiadk@users.noreply.github.com>
2024-02-13 08:33:18 +0700

    Update GaleriController.php

--- app/Http/Controllers/GaleriController.php
+++ app/Http/Controllers/GaleriController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Statik;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\File;
 
 /**
   * @group Galeri
