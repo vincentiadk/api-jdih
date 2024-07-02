ddcd443781bd9bb5d1e4413a287754c5c603002d
vincentiadk <vincentiadksitanggang@gmail.com>
2024-07-02 22:50:29 +0700

    db inlis

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,6 +13,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->register(\Yajra\Oci8\Oci8ServiceProvider::class);
+        //$this->app->register(\Yajra\Oci8\Oci8ServiceProvider::class);
     }
 }
--- config/database.php
+++ config/database.php
@@ -4,17 +4,18 @@ return [
 
     'connections' => [
         'inlis' => [
-            'driver'    => 'oracle',
-            'tns'       => env('DB_INLIS_TNS', ''),
-            'host'      => env('DB_INLIS_HOST', ''),
-            'port'      => env('DB_INLIS_PORT', '1521'),
+            'driver'    => 'mysql',
+            'host'      => env('DB_INLIS_HOST', '127.0.0.1'),
+            'port'      => env('DB_INLIS_PORT', '3306'),
             'database'  => env('DB_INLIS_DATABASE', ''),
             'username'  => env('DB_INLIS_USERNAME', ''),
             'password'  => env('DB_INLIS_PASSWORD', ''),
-            'charset'   => env('DB_INLIS_CHARSET', 'AL32UTF8'),
-            'prefix'    => env('DB_INLIS_PREFIX', ''),
-            'prefix_schema' => env('DB_INLIS_SCHEMA_PREFIX', ''),
-            'edition'   => env('DB_EDITION', 'ora$base'),
+            'charset'   => env('DB_INLIS_CHARSET', 'utf8mb4'),
+            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
+            'prefix' => env('DB_PREFIX', ''),
+            'strict' => env('DB_STRICT_MODE', true),
+            'engine' => env('DB_ENGINE', null),
+            'timezone' => env('DB_TIMEZONE', '+00:00'),
         ],
         'mysql' => [
             'driver' => 'mysql',
