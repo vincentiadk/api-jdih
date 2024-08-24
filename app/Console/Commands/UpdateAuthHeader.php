5eeb8578dee44b514dd84bb0bbd69a8aa00bfd74
juaracoding2424 <vd.tyaz@gmail.com>
2024-08-24 17:40:17 +0700

    5 - 11

--- app/Console/Commands/UpdateAuthHeader.php
+++ app/Console/Commands/UpdateAuthHeader.php
@@ -122,7 +122,7 @@ class UpdateAuthHeader extends Command
             ];
             $i = 1;
             foreach($datas as $d){
-                $user = $datauser[random_int(0,4)];
+                $user = $datauser[random_int(5,10)];
                 $cDate = $this->getValidateDate($user['user'], $min_sec, $max_sec);
                 $items =  [ ["name" => 'CREATEBY', "Value"=> $user["user"]],
                             ["name" => 'CREATEDATE', "Value"=> $cDate],
