8080e657e41a2d4ffb8b757e10df8ce95ca034eb
juaracoding2424 <vd.tyaz@gmail.com>
2024-08-24 10:00:10 +0700

    Update UpdateAuthHeader.php

--- app/Console/Commands/UpdateAuthHeader.php
+++ app/Console/Commands/UpdateAuthHeader.php
@@ -133,7 +133,7 @@ class UpdateAuthHeader extends Command
                 "id" => $d["AUTH_HEADER_ID"],
                 "ListUpdateItem"=> json_encode($items)
             ]);
-            $out->writeln($i . " " . $response['Message'] . " ID => " . $d['AUTH_HEADER_ID'] . " User => " . $user['user']);
+            $out->writeln($i . " " . $response['Message'] . " ID => " . $d['AUTH_HEADER_ID'] . " User => " . $user['user'] . "Date => " . $cDate);
             $i++;
         }
         
@@ -167,5 +167,6 @@ class UpdateAuthHeader extends Command
             return $return;
         }
     }
+
 
 }
