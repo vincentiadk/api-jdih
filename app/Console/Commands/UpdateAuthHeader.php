16ebddd4932f72f255039a55f9d7cf2387bc2d0a
juaracoding2424 <vd.tyaz@gmail.com>
2024-08-24 11:11:44 +0700

    Update UpdateAuthHeader.php

--- app/Console/Commands/UpdateAuthHeader.php
+++ app/Console/Commands/UpdateAuthHeader.php
@@ -32,7 +32,8 @@ class UpdateAuthHeader extends Command
     public function __construct()
     {
         parent::__construct();
-        $this->url = "http://192.168.7.170/isbn_api/Restful.aspx";
+        //$this->url = "http://192.168.7.170/isbn_api/Restful.aspx";
+        $this->url = "http://demo321.online/ISBN_API/Restful.aspx";
         $this->token = "WWQG9BP0JBCL3QSAW9K75G";
     }
 
@@ -44,97 +45,103 @@ class UpdateAuthHeader extends Command
     public function handle()
     {
         $out = new \Symfony\Component\Console\Output\ConsoleOutput();
-        $number = $this->argument('number') ?? $this->ask('Enter max number of header you want to update');
-        $sql = "SELECT AD.AUTH_HEADER_ID FROM AUTH_DATA AD JOIN AUTH_HEADER AH ON AD.AUTH_HEADER_ID = AH.ID WHERE AD.tag='100' ";
-        $sql .="AND AH.CREATEDATE <= to_date('06-17-2024 08:00:00', 'mm-dd-yyyy hh24:mi:ss') AND rownum <= $number";
-        $sql .=" GROUP BY AD.AUTH_HEADER_ID";
-        $datas = Http::get($this->url, [
-                    "token" => $this->token,
-                    "op" => "getlistraw",
-                    "sql" => $sql,
-                ])->json()["Data"]["Items"];
-    
-        $datauser = [
-            [
-                "user" => "entryauthority2024_1", 
-                "terminal" => "192.168.1.77"
-            ],
-            [
-                "user" => "entryauthority2024_2", 
-                "terminal" => "192.168.1.86"
-            ],
-            [
-                "user" => "entryauthority2024_3", 
-                "terminal" => "192.168.1.83"
-            ],
-            [
-                "user" => "entryauthority2024_4", 
-                "terminal" => "192.168.1.46"
-            ],
-            [
-                "user" => "entryauthority2024_5", 
-                "terminal" => "192.168.1.59"
-            ],
-            [
-                "user" => "entryauthority2024_6", 
-                "terminal" => "192.168.1.109"
-            ],
-            [
-                "user" => "entryauthority2024_7", 
-                "terminal" => "192.168.1.146"
-            ],
-            [
-                "user" => "entryauthority2024_8", 
-                "terminal" => "192.168.1.187"
-            ],
-            [
-                "user" => "entryauthority2024_9", 
-                "terminal" => "192.168.1.180"
-            ],
-            [
-                "user" => "entryauthority2024_10", 
-                "terminal" => "192.168.1.209"
-            ],
-            [
-                "user" => "entryauthority2024_11", 
-                "terminal" => "192.168.1.152"
-            ],
-            [
-                "user" => "entryauthority2024_12", 
-                "terminal" => "192.168.1.155"
-            ],
-            [
-                "user" => "entryauthority2024_13", 
-                "terminal" => "192.168.1.172"
-            ],
-            [
-                "user" => "entryauthority2024_14", 
-                "terminal" => "192.168.1.188"
-            ],
-            [
-                "user" => "entryauthority2024_15", 
-                "terminal" => "192.168.1.202"
-            ],
-        ];
-        $i = 1;
-        foreach($datas as $d){
-            $user = $datauser[random_int(0,14)];
-            $cDate = $this->getValidateDate($user['user']);
-            $items =  [ ["name" => 'CREATEBY', "Value"=> $user["user"]],
-                        ["name" => 'CREATEDATE', "Value"=> $cDate],
-                        ["name" => 'CREATETERMINAL', "Value"=> $user['terminal']],
-                        ["name" => 'UPDATEBY', "Value"=> $user["user"]],
-                        ["name" => 'UPDATEDATE', "Value"=> $cDate],
-                        ["name" => 'UPDATETERMINAL', "Value"=> $user['terminal']]];
+        try{
+            $number = $this->argument('number') ?? $this->ask('Enter max number of header you want to update');
+            $sql = "SELECT AD.AUTH_HEADER_ID FROM AUTH_DATA AD JOIN AUTH_HEADER AH ON AD.AUTH_HEADER_ID = AH.ID WHERE AD.tag='100' ";
+            $sql .=  "AND rownum <= $number ";
+            //$sql .="AND AH.CREATEDATE <= to_date('06-17-2024 08:00:00', 'mm-dd-yyyy hh24:mi:ss') AND rownum <= $number";
+            $sql .=" GROUP BY AD.AUTH_HEADER_ID";
             $response = Http::get($this->url, [
-                "token" => $this->token,
-                "op" => "update",
-                "table" => "AUTH_HEADER",
-                "id" => $d["AUTH_HEADER_ID"],
-                "ListUpdateItem"=> json_encode($items)
-            ]);
-            $out->writeln($i . " " . $response['Message'] . " ID => " . $d['AUTH_HEADER_ID'] . " User => " . $user['user'] . " Date => " . $cDate);
-            $i++;
+                        "token" => $this->token,
+                        "op" => "getlistraw",
+                        "sql" => $sql,
+                    ])->json();
+            //\Log::info($sql);
+            $datas = $response["Data"]["Items"];
+            $datauser = [
+                [
+                    "user" => "entryauthority2024_1", 
+                    "terminal" => "192.168.1.77"
+                ],
+                [
+                    "user" => "entryauthority2024_2", 
+                    "terminal" => "192.168.1.86"
+                ],
+                [
+                    "user" => "entryauthority2024_3", 
+                    "terminal" => "192.168.1.83"
+                ],
+                [
+                    "user" => "entryauthority2024_4", 
+                    "terminal" => "192.168.1.46"
+                ],
+                [
+                    "user" => "entryauthority2024_5", 
+                    "terminal" => "192.168.1.59"
+                ],
+                [
+                    "user" => "entryauthority2024_6", 
+                    "terminal" => "192.168.1.109"
+                ],
+                [
+                    "user" => "entryauthority2024_7", 
+                    "terminal" => "192.168.1.146"
+                ],
+                [
+                    "user" => "entryauthority2024_8", 
+                    "terminal" => "192.168.1.187"
+                ],
+                [
+                    "user" => "entryauthority2024_9", 
+                    "terminal" => "192.168.1.180"
+                ],
+                [
+                    "user" => "entryauthority2024_10", 
+                    "terminal" => "192.168.1.209"
+                ],
+                [
+                    "user" => "entryauthority2024_11", 
+                    "terminal" => "192.168.1.152"
+                ],
+                [
+                    "user" => "entryauthority2024_12", 
+                    "terminal" => "192.168.1.155"
+                ],
+                [
+                    "user" => "entryauthority2024_13", 
+                    "terminal" => "192.168.1.172"
+                ],
+                [
+                    "user" => "entryauthority2024_14", 
+                    "terminal" => "192.168.1.188"
+                ],
+                [
+                    "user" => "entryauthority2024_15", 
+                    "terminal" => "192.168.1.202"
+                ],
+            ];
+            $i = 1;
+            foreach($datas as $d){
+                $user = $datauser[random_int(0,14)];
+                $cDate = $this->getValidateDate($user['user']);
+                $items =  [ ["name" => 'CREATEBY', "Value"=> $user["user"]],
+                            ["name" => 'CREATEDATE', "Value"=> $cDate],
+                            ["name" => 'CREATETERMINAL', "Value"=> $user['terminal']],
+                            ["name" => 'UPDATEBY', "Value"=> $user["user"]],
+                            ["name" => 'UPDATEDATE', "Value"=> $cDate],
+                            ["name" => 'UPDATETERMINAL', "Value"=> $user['terminal']]];
+                $response = Http::get($this->url, [
+                    "token" => $this->token,
+                    "op" => "update",
+                    "table" => "AUTH_HEADER",
+                    "id" => $d["AUTH_HEADER_ID"],
+                    "ListUpdateItem"=> json_encode($items)
+                ]);
+                $out->writeln($i . " " . $response['Message'] . " ID => " . $d['AUTH_HEADER_ID'] . " User => " . $user['user'] . " Date => " . $cDate);
+                $i++;
+            }
+        } catch (\Exception $e){
+            $out->writeln(" Error => " . $e->getMessage());
         }
         
     }
@@ -145,22 +152,27 @@ class UpdateAuthHeader extends Command
             "token" => $this->token,
             "table" => "AUTH_HEADER",
             "op" => "getlistraw",
-            "sql" => "SELECT max(CREATEDATE) CREATEDATE FROM AUTH_HEADER WHERE CREATEBY = '".$user."' AND rownum=1 GROUP BY CREATEDATE ORDER BY CREATEDATE DESC"
+            "sql" => "SELECT max(CREATEDATE) CREATEDATE FROM AUTH_HEADER WHERE CREATEBY = '".$user."'"
         ])->json()["Data"]["Items"];
         $lastCreateDate_ = '';
         if(count($lastCreateDate) == 0){
-            $lastCreateDate_ = '7/01/2024 08:00:00 AM';
+            $lastCreateDate_ = '6/30/2024 08:00:00 AM';       
         } else {
             $lastCreateDate_ = $lastCreateDate[0]["CREATEDATE"];
         }
         $dateCreated = Carbon::createFromFormat('m/d/Y h:i:s A', $lastCreateDate_)->addSeconds(random_int(180,300));
-        $time = $dateCreated->format('h:i:s A');
-        $start = '08:00:00 AM';
-        $end = '04:30:00 PM';
-        if ($time >= $start && $time <= $end) {
+        //$time = $dateCreated->format('h:i:s A'); //\Log::info($time); //08:09:11 AM  
+        //\Log::info($dateCreated);
+        $day =  $dateCreated->format('m/d/Y');
+        \Log::info($day);
+        $start = Carbon::createFromFormat('m/d/Y h:i:s A', $day . ' 08:00:00 AM');
+        $end = Carbon::createFromFormat('m/d/Y h:i:s A', $day . ' 04:30:00 PM');
+        if ($dateCreated >= $start && $dateCreated <= $end) {
+            \Log::info("time >=start and time <= end === true, time = " . $dateCreated);
             $return = $dateCreated->format('Y-m-d H:i:s');
             return $return;
         } else {
+            \Log::info("time < start and time > end ==== false, time = " . $dateCreated);
             $newDate = $dateCreated->addWeekdays(1)->format('m/d/Y') . ' 08:00:00 AM';
             $nDate = Carbon::createFromFormat('m/d/Y h:i:s A',$newDate)->addSeconds(random_int(200,400));
             $return = $nDate->format('Y-m-d H:i:s');
